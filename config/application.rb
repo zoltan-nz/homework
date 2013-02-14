266773d70118fe8ff333160d2e03cfdad549c72f
Zoltan <zdebre@gmail.com>
2013-02-14 19:19:07 +0000

    Configuration is done.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Homework
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Sass is default
+    config.sass.preferred_syntax = :sass
   end
 end
