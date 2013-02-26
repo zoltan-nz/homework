6f7d9102e332e3779471e976d83bcfc17fb69f5f
Zoltan <zdebre@gmail.com>
2013-02-26 19:25:56 +0000

    model update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,6 +10,8 @@ class Booking < ActiveRecord::Base
 
   #Result: how many bed booked on a day
   scope :number_of_bed_booked, lambda {|day| where("? BETWEEN bookings.start_date AND bookings.end_date", day).select("number_of_guests")}
+  scope :booked_in_a_period, lambda  {|sd,ed| where("(:sd BETWEEN start_date AND end_date) OR (:ed BETWEEN start_date AND end_date) OR ((:sd < start_date) AND ( end_date < :ed))", sd: sd, ed: ed)}
+
 
 
   #Result: integer - a max number of how many bed booked in a period
